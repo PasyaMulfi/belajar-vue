5759bcc37f69873b289df505a23e798332ec3446
PasyaMulfi <pasyamulfi@gmail.com>
2023-07-20 15:33:57 +0700

    ini perkenalan vue.js

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref("Info Pendaftaran Barudak well");
+let rawHtml = "<li> Punya Spokat Nyala</li> <li>Jaket Gelembung</li> <li> Ripped Jeans</li> "
+const objectOfAttrs = {
+  id: 'container',
+  class: 'wrapper'
+};
+let seen = " "
+
+let a = 10
+let b = 5
+let c = a+b
+</script>
+<template>
+
+<form @submit.prevent="onSubmit">
+  <div>
+    <h1>{{ msg }} </h1>
+    <div v-bind="objectofAttrs">Ketentuan masuk:</div>
+    <span v-html="rawHtml"></span>
+    <p v-if="seen">minimal harus bikin jj pake tongkat baseball</p>
+  <button :disabled="isButtonDisabled">Button</button>&ensp;    
+  <button :disabled="isButtonDisabled">Button</button>
+
+  <h1>ARITMATIKA</h1>
+  bil={{ a }} bil={{ b }}<br> hasil bil1 {{ a }} + bil2 {{ b }}= {{ c }}
+  
+  </div>
+  </form>
+</template>
